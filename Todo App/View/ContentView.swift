e83739cfa263643b2a7e864b30722393ceaebf8a
muhammad mufti ramdhani <muftiramdhani25@gmail.com>
2021-02-16 11:24:42 +0700

    Hari Kedua

--- Todo App/View/ContentView.swift
+++ Todo App/View/ContentView.swift
@@ -8,30 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var showingAddTodoView: Bool = false
-    
-    var body: some View {
-        NavigationView{
-            List(0..<5){ item in
-                Text("Hello World")
-            }
-            .navigationBarTitle("Todo", displayMode: .inline)
-            .navigationBarItems(trailing:
-                                    Button(action: {
-                                        self.showingAddTodoView.toggle()
-                                    }){
-                                        Image(systemName: "plus")
-                                    }
-                                    .sheet(isPresented: $showingAddTodoView){
-                                        AddTodoView()
-                                    })
-        }
+  
+  @Environment(\.managedObjectContext) var managedObjectContext
+  
+  @State private var showingAddTodoView: Bool = false
+  
+  var body: some View {
+    NavigationView{
+      List(0..<5){ item in
+        Text("Hello World")
+      }
+      .navigationBarTitle("Todo", displayMode: .inline)
+      .navigationBarItems(trailing:
+                            Button(action: {
+                              self.showingAddTodoView.toggle()
+                            }){
+                              Image(systemName: "plus")
+                            }
+        .sheet(isPresented: $showingAddTodoView){
+          AddTodoView().environment(\.managedObjectContext, self.managedObjectContext)
+        })
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
